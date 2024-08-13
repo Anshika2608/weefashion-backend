a1183fa128551b729340ff0082b506b5bc0f65cc
Anshika2608 <anshikamittal1910@gmail.com>
2024-08-13 20:30:50 +0530

    multiple allowed origins

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require("express");
+require("./config/config");
+const cors = require("cors");
+const cookieParser=require("cookie-parser")
+const app = express();
+app.use(express.json());
+
+const path=require('path')
+const allowedOrigins = [
+  'https://weefashion-shopping-frontend.onrender.com',
+  'http://localhost:5173/',
+];
+app.use(cors({
+  origin: allowedOrigins, 
+  credentials: true
+}));
+app.use(cookieParser())
+const port=process.env.PORT || 3000;
+
+app.use("/api/home", require("./routes/Homeroutes"));
+app.use("/api/Clothing",require("./routes/Clothingroutes"))
+app.use("/api/wishlist",require("./routes/WishlistRoutes"))
+app.use("/api/signup", require("./routes/Authenticationroutes"));
+app.use("/api/login", require("./routes/Authenticationroutes"));
+app.use("/api/validUser",require("./routes/Authenticationroutes"))
+app.use("/api/logoutUser",require("./routes/Authenticationroutes"))
+app.use("/api/changePassword",require("./routes/Authenticationroutes"))
+app.use("/api/change",require("./routes/Authenticationroutes"))
+app.use("/api/verifyForgot",require("./routes/Authenticationroutes"))
+app.use("/api/cart",require("./routes/CartRoutes"))
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
